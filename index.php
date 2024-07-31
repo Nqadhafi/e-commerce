dcc2d99cc24901995aadfc64bb31833a6c771421
Nqadhafi <qadhafi817@gmail.com>
2024-07-31 21:48:18 +0700

    Index catalog

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 include('header.php');
+include('config.php');
+$query = mysqli_query($config, "SELECT * FROM tb_produk");
+$data = mysqli_fetch_all($query, MYSQLI_ASSOC);
+
 ?>
 <body>
     <div class="container-fluid">
@@ -11,62 +15,19 @@ include('header.php');
         <!-- katalog -->
         <div class="container">
             <div class="row justify-content-center">
+            <?php foreach ($data as $data) : ?>
                 <!-- Produk -->
                 <div class="col-md-4 d-flex justify-content-center mb-4">
-                    <div class="card" style="width: 15rem;">
-                        <img src="..." class="card-img-top front-display" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Produk anjay</h5>
-                            <p class="card-text harga">Rp.50.000</p>
+                    <div class="card border-secondary border" style="width: 15rem;">
+                        <img src="./assets/uploads/<?php echo $data['gambar_produk'] ?>" class="card-img-top front-display p-2" alt="...">
+                        <div class="card-body border-top border-secondary">
+                            <h5 class="card-title"><?php echo $data['nama_produk']?></h5>
+                            <p class="card-text harga">Rp.<?php echo $data['harga_produk']?></p>
                         </div>
                     </div>
                 </div>
                 <!-- Produk End -->
-                <!-- Produk -->
-                <div class="col-md-4 d-flex justify-content-center mb-4">
-                    <div class="card" style="width: 15rem;">
-                        <img src="..." class="card-img-top front-display" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text harga">Rp.50.000</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Produk End -->
-                <!-- Produk -->
-                <div class="col-md-4 d-flex justify-content-center mb-4">
-                    <div class="card" style="width: 15rem;">
-                        <img src="..." class="card-img-top front-display" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text harga">Rp.50.000</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Produk End -->
-                   <!-- Produk -->
-                <div class="col-md-4 d-flex justify-content-center mb-4">
-                    <div class="card" style="width: 15rem;">
-                        <img src="..." class="card-img-top front-display" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text harga">Rp.50.000</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Produk End -->
-                   <!-- Produk -->
-                <div class="col-md-4 d-flex justify-content-center mb-4">
-                    <div class="card" style="width: 15rem;">
-                        <img src="..." class="card-img-top front-display" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text harga">Rp.50.000</p>
-                        </div>
-                    </div>
-                </div>
-                <!-- Produk End -->
-                 
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
