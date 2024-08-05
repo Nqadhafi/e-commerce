f6178972f9df760795664c7a8ec4e3502ce8ea37
Nqadhafi <qadhafi817@gmail.com>
2024-08-05 11:31:34 +0700

    add sql

--- toko_onlineku.sql
+++ toko_onlineku.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 04, 2024 at 09:31 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Generation Time: Aug 05, 2024 at 06:30 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -25,108 +25,41 @@ USE `toko_onlineku`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tb_keranjang`
+-- Table structure for table `tb_admin`
 --
 
-CREATE TABLE `tb_keranjang` (
-  `id` int(11) NOT NULL,
-  `id_keranjang` varchar(50) NOT NULL,
-  `id_produk` int(11) NOT NULL,
-  `qty_keranjang` int(11) NOT NULL,
-  `subtotal_keranjang` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `tb_admin` (
+  `id_admin` int(11) NOT NULL,
+  `user_admin` varchar(50) NOT NULL,
+  `password_admin` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `tb_keranjang`
+-- Dumping data for table `tb_admin`
 --
 
-INSERT INTO `tb_keranjang` (`id`, `id_keranjang`, `id_produk`, `qty_keranjang`, `subtotal_keranjang`) VALUES
-(51, '#66afd3323bee8', 20, 1, 3000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_order`
---
-
-CREATE TABLE `tb_order` (
-  `id_order` varchar(50) NOT NULL,
-  `namacust_order` varchar(50) NOT NULL,
-  `email_order` varchar(50) NOT NULL,
-  `nohp_order` bigint(20) NOT NULL,
-  `alamat_order` varchar(100) NOT NULL,
-  `grandtotal_order` bigint(20) NOT NULL,
-  `status_order` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tb_order`
---
-
-INSERT INTO `tb_order` (`id_order`, `namacust_order`, `email_order`, `nohp_order`, `alamat_order`, `grandtotal_order`, `status_order`) VALUES
-('#66afd3323bee8', 'asd', '123@asdasd', 123123, 'asdasd', 3000, 'Pending');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_produk`
---
-
-CREATE TABLE `tb_produk` (
-  `id_produk` int(11) NOT NULL,
-  `nama_produk` varchar(30) NOT NULL,
-  `harga_produk` int(10) NOT NULL,
-  `deskripsi_produk` varchar(50) NOT NULL,
-  `gambar_produk` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tb_produk`
---
-
-INSERT INTO `tb_produk` (`id_produk`, `nama_produk`, `harga_produk`, `deskripsi_produk`, `gambar_produk`) VALUES
-(19, 'Kubernetes', 50000, 'Joki kubernetes', '66abb15819ea9.jpg'),
-(20, 'Orang hitam', 3000, 'Anjay nigga', '66aa4bbb60c10.jpg'),
-(21, 'Stiker Vinyl', 7500, 'Ini stiker coy', '66aba71a8f2f0.png');
+INSERT INTO `tb_admin` (`id_admin`, `user_admin`, `password_admin`) VALUES
+(1, 'admin', 'tokoonline123');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `tb_keranjang`
+-- Indexes for table `tb_admin`
 --
-ALTER TABLE `tb_keranjang`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
---
--- Indexes for table `tb_order`
---
-ALTER TABLE `tb_order`
-  ADD PRIMARY KEY (`id_order`);
-
---
--- Indexes for table `tb_produk`
---
-ALTER TABLE `tb_produk`
-  ADD PRIMARY KEY (`id_produk`);
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `tb_keranjang`
+-- AUTO_INCREMENT for table `tb_admin`
 --
-ALTER TABLE `tb_keranjang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
-
---
--- AUTO_INCREMENT for table `tb_produk`
---
-ALTER TABLE `tb_produk`
-  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+ALTER TABLE `tb_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
