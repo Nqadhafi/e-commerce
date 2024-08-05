4c7c01c05617afce4605c5ef5eaf71304777763b
Nqadhafi <qadhafi817@gmail.com>
2024-08-05 11:35:59 +0700

    add sql(2)

--- toko_onlineku.sql
+++ toko_onlineku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2024 at 06:30 AM
+-- Generation Time: Aug 05, 2024 at 06:35 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,6 +41,75 @@ CREATE TABLE `tb_admin` (
 INSERT INTO `tb_admin` (`id_admin`, `user_admin`, `password_admin`) VALUES
 (1, 'admin', 'tokoonline123');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keranjang`
+--
+
+CREATE TABLE `tb_keranjang` (
+  `id` int(11) NOT NULL,
+  `id_keranjang` varchar(50) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `qty_keranjang` int(11) NOT NULL,
+  `subtotal_keranjang` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_keranjang`
+--
+
+INSERT INTO `tb_keranjang` (`id`, `id_keranjang`, `id_produk`, `qty_keranjang`, `subtotal_keranjang`) VALUES
+(51, '#66afd3323bee8', 20, 1, 3000),
+(52, '#66b04b659a412', 19, 2, 100000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_order`
+--
+
+CREATE TABLE `tb_order` (
+  `id_order` varchar(50) NOT NULL,
+  `namacust_order` varchar(50) NOT NULL,
+  `email_order` varchar(50) NOT NULL,
+  `nohp_order` bigint(20) NOT NULL,
+  `alamat_order` varchar(100) NOT NULL,
+  `grandtotal_order` bigint(20) NOT NULL,
+  `status_order` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_order`
+--
+
+INSERT INTO `tb_order` (`id_order`, `namacust_order`, `email_order`, `nohp_order`, `alamat_order`, `grandtotal_order`, `status_order`) VALUES
+('#66afd3323bee8', 'asd', '123@asdasd', 123123, 'asdasd', 3000, 'Pending'),
+('#66b04b659a412', 'asdasd@asdasd', 'nandaqadafi@yahoo.co.id', 1123, 'asdasd', 100000, 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_produk`
+--
+
+CREATE TABLE `tb_produk` (
+  `id_produk` int(11) NOT NULL,
+  `nama_produk` varchar(30) NOT NULL,
+  `harga_produk` int(10) NOT NULL,
+  `deskripsi_produk` varchar(50) NOT NULL,
+  `gambar_produk` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_produk`
+--
+
+INSERT INTO `tb_produk` (`id_produk`, `nama_produk`, `harga_produk`, `deskripsi_produk`, `gambar_produk`) VALUES
+(19, 'Kubernetes', 50000, 'Joki kubernetes', '66abb15819ea9.jpg'),
+(20, 'Orang hitam', 3000, 'Anjay nigga', '66aa4bbb60c10.jpg'),
+(21, 'Stiker Vinyl', 7500, 'Ini stiker coy', '66aba71a8f2f0.png');
+
 --
 -- Indexes for dumped tables
 --
@@ -52,6 +121,25 @@ ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
+-- Indexes for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `tb_order`
+--
+ALTER TABLE `tb_order`
+  ADD PRIMARY KEY (`id_order`);
+
+--
+-- Indexes for table `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  ADD PRIMARY KEY (`id_produk`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +148,18 @@ ALTER TABLE `tb_admin`
 --
 ALTER TABLE `tb_admin`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `tb_produk`
+--
+ALTER TABLE `tb_produk`
+  MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
