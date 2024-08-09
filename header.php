e8d6717e9b5fe33a76827339e373e80bfb292429
Nqadhafi <qadhafi817@gmail.com>
2024-08-09 20:30:53 +0700

    navbar

--- header.php
+++ header.php
@@ -31,7 +31,7 @@ if (isset($_SESSION['keranjang'])) {
         <img src="./assets/img/logo-utama.png" alt="Note Pro" class="w-50">
         </div>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
            
